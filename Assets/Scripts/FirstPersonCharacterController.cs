bbf1fd0cd214b578197f7497f837662a07b29d11
hayden-donnelly <hayden.git@proton.me>
2018-12-01 10:23:49 -0500

    edits

--- Assets/Scripts/FirstPersonCharacterController.cs
+++ Assets/Scripts/FirstPersonCharacterController.cs
@@ -5,19 +5,41 @@ using UnityEngine;
 public class FirstPersonCharacterController : MonoBehaviour 
 {
 	[SerializeField] private float walkSpeed;
+	[SerializeField] private float crouchSpeed;
 	[SerializeField] private float sensitivityX;
 	[SerializeField] private float sensitivityY;
 	[SerializeField] private CharacterController controller;
 	[SerializeField] private Camera mainCamera;
 
+	private bool isCrouched;
+	private float moveSpeed;
+
 	private void Start()
 	{
 		controller = GetComponent<CharacterController>();
 		mainCamera = Camera.main;
+
+		moveSpeed = walkSpeed;
 	}
 
 	private void Update()
 	{
+		// Crouching
+		if(Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			if(isCrouched)
+			{
+				isCrouched = false;
+				moveSpeed = walkSpeed;
+			}
+			else
+			{
+				isCrouched = true;
+				moveSpeed = crouchSpeed;
+			}
+		}
+
+		// Rotation
 		float mouseX = Input.GetAxis("Mouse X");
 		mouseX *= sensitivityX * Time.deltaTime;
 		transform.Rotate(Vector3.up, mouseX);
@@ -25,12 +47,13 @@ public class FirstPersonCharacterController : MonoBehaviour
 		mouseY *= sensitivityY * Time.deltaTime;
 		mainCamera.transform.Rotate(-Vector3.right, mouseY);
 
+		// Movement
 		float inputX = Input.GetAxisRaw("Horizontal");
 		float inputY = Input.GetAxisRaw("Vertical");
 		Vector3 movement = new Vector3(inputX, 0, inputY);
 		movement.Normalize();
 		movement = transform.TransformDirection(movement);
-		movement *= walkSpeed * Time.deltaTime;
+		movement *= moveSpeed * Time.deltaTime;
 		controller.Move(movement);
 	}
 }
