18e7daae8c80ea78c9f73da879bb06d7b76eb117
Hayden Donnelly <Bizbud@live.com>
2018-12-01 10:14:55 -0500

    Created first person character controller

--- Assets/Scripts/FirstPersonCharacterController.cs
+++ Assets/Scripts/FirstPersonCharacterController.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 public class FirstPersonCharacterController : MonoBehaviour 
 {
 	[SerializeField] private float walkSpeed;
+	[SerializeField] private float sensitivityX;
+	[SerializeField] private float sensitivityY;
 	[SerializeField] private CharacterController controller;
+	[SerializeField] private Camera mainCamera;
 
 	private void Start()
 	{
 		controller = GetComponent<CharacterController>();
+		mainCamera = Camera.main;
 	}
 
 	private void Update()
 	{
+		float mouseX = Input.GetAxis("Mouse X");
+		mouseX *= sensitivityX * Time.deltaTime;
+		transform.Rotate(Vector3.up, mouseX);
+		float mouseY = Input.GetAxis("Mouse Y");
+		mouseY *= sensitivityY * Time.deltaTime;
+		mainCamera.transform.Rotate(-Vector3.right, mouseY);
+
 		float inputX = Input.GetAxisRaw("Horizontal");
 		float inputY = Input.GetAxisRaw("Vertical");
 		Vector3 movement = new Vector3(inputX, 0, inputY);
 		movement.Normalize();
+		movement = transform.TransformDirection(movement);
 		movement *= walkSpeed * Time.deltaTime;
 		controller.Move(movement);
 	}
