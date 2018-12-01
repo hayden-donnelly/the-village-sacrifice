57cc1a5e6fa806891f871e0631d3503146979771
hayden-donnelly <hayden.git@proton.me>
2018-12-01 13:43:43 -0500

    AI tweaks

--- Assets/Scripts/EnemyAIController.cs
+++ Assets/Scripts/EnemyAIController.cs
@@ -8,28 +8,63 @@ public class EnemyAIController : MonoBehaviour
 	[SerializeField] private NavMeshAgent agent;
 	[SerializeField] private Transform player;
 	[SerializeField] private LayerMask mask;
+	[SerializeField] private Transform patrolRouteParent;
+	[SerializeField] private List<Transform> patrolRoute = new List<Transform>();
+
+	private int patrolRouteIndex;
 
 	private void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
 		player = GameObject.FindGameObjectWithTag("Player").transform;
+
+		SetupPatrolRoute();
+		StartCoroutine(followRoute());
 	} 
 
 	private void Update()
 	{
 		Debug.DrawRay(transform.position, transform.forward * 5, Color.blue);
 		
-		//if(Input.GetKeyDown(KeyCode.Space))
-		//{
-		//	agent.SetDestination(player.position);
-		//}
-
+		
 		if(!Physics.Linecast(transform.position, player.position, mask))
 		{
-			if(Vector3.Angle(transform.forward, player.transform.position) < 120)
+			if(Vector3.Angle(transform.forward, player.transform.position) < 60)
 			{
+				StopCoroutine(followRoute());
 				agent.SetDestination(player.position);
 			}
+		}
+	}
+
+	private void SetupPatrolRoute()
+	{
+		for(int i = 0; i < patrolRouteParent.childCount; i++)
+		{
+			patrolRoute.Add(patrolRouteParent.GetChild(i));
+		}
+	}
+
+	private IEnumerator followRoute()
+	{
+		agent.SetDestination(patrolRoute[0].position);
+
+		while(true)
+		{
+			print("dsdasdasd");
+			agent.SetDestination(patrolRoute[patrolRouteIndex].position);
+
+			while(Vector3.Distance(agent.destination, transform.position) > 3)
+			{
+				yield return null;
+			}
+
+			patrolRouteIndex++;
+			if(patrolRouteIndex >= patrolRoute.Count)
+			{
+				patrolRouteIndex = 0;
+			}
+			yield return null;
 		}
 	}
 }
