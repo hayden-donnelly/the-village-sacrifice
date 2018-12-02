0ac8c084be969b305f2ec950c2669cddbdac193f
hayden-donnelly <hayden.git@proton.me>
2018-12-01 20:16:10 -0500

    Added switching betweem patrol and chase mode

--- Assets/Scripts/EnemyAIController.cs
+++ Assets/Scripts/EnemyAIController.cs
@@ -5,25 +5,55 @@ using UnityEngine.AI;
 
 public class EnemyAIController : MonoBehaviour 
 {
-	[SerializeField] private Transform player;
+	[SerializeField] private int fieldOfView;
 	[SerializeField] private LayerMask mask;
+	[HideInInspector] public NavMeshAgent agent;
+	[HideInInspector] public Transform player;
+	private BaseState state;
+	private BaseState[] availableStates;
 
 	private void Start()
 	{
+		agent = GetComponent<NavMeshAgent>();
 		player = GameObject.FindGameObjectWithTag("Player").transform;
+		availableStates = GetComponents<BaseState>();
+		state = GetComponent<PatrolState>();
+		state.Construct();
 	} 
 
 	private void Update()
 	{
-		Debug.DrawRay(transform.position, transform.forward * 5, Color.blue);
-		
-		
-		if(!Physics.Linecast(transform.position, player.position, mask))
+		state.Transition();
+	}
+
+	public void ChangeState(string stateName)
+	{
+		foreach(BaseState s in availableStates)
 		{
-			if(Vector3.Angle(transform.forward, player.transform.position) < 60)
+			if(s.GetType().FullName != stateName)
 			{
-				//HERE!
+				continue;
 			}
+
+			if(s.unlocked)
+			{
+				state.Destruct();
+				state = s;
+				state.Construct();
+			}
+			return;
 		}
+
+		Debug.LogWarning("New state could not be found");
+	}
+
+	public bool CanSeePlayer()
+	{
+		if(!Physics.Linecast(transform.position, player.position, mask)
+			&& Vector3.Angle(transform.forward, player.transform.position) < fieldOfView)
+		{
+			return true;
+		}
+		return false;
 	}
 }
