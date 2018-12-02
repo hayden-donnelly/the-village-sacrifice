c486d7f1b6ca1ae4101ca5a29d459240d1b51c3c
hayden-donnelly <hayden.git@proton.me>
2018-12-02 12:07:27 -0500

    Smarter search behaviour

--- Assets/Scripts/Enemy States/SearchState.cs
+++ Assets/Scripts/Enemy States/SearchState.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class SearchState : BaseState 
 {
     [SerializeField] private float searchRadius;
+    [SerializeField] private float searchDistance;
 
     public override void Construct()
     {
+        Debug.Log("Entering Search State");
         coroutine = Search();
         StartCoroutine(coroutine);
     }
@@ -23,6 +25,7 @@ public class SearchState : BaseState
         int searchRouteIndex = 0;
         while(true)
         {
+            // Follow search route
             motor.agent.SetDestination(searchRoute[searchRouteIndex]);
             while(Vector3.Distance(transform.position, motor.agent.destination) > 2)
             {
@@ -33,35 +36,27 @@ public class SearchState : BaseState
             if(searchRouteIndex >= searchRoute.Count)
             {
                 motor.ChangeState("PatrolState");  
-                break;  // TODO may not be needed  
             }
             yield return null;
         }
-        yield return null;
     }
 
     private List<Vector3> GenerateSearchRoute(Vector3 lastSeenPosition)
-    {
+    {   
+        // Predict the position that the player will move to
+        Vector3 playerMovement = motor.player.Controller.velocity;
+        playerMovement = new Vector3(playerMovement.x, 0, playerMovement.z);
+        playerMovement.Normalize();
+        playerMovement *= searchDistance;
+        Vector3 predictedPlayerPosition = lastSeenPosition + playerMovement;
+
         List<Vector3> searchRoute = new List<Vector3>();
         searchRoute.Add(lastSeenPosition);
-
-        /*int numberOfNodes = 3;
-        
-        for(int i = 0 ; i < numberOfNodes; i++)
-        {
-            float minX = lastSeenPosition.x - searchRadius;
-            float maxX = lastSeenPosition.x + searchRadius;
-            float minY = lastSeenPosition.y - searchRadius;
-            float maxY = lastSeenPosition.y + searchRadius;
-
-            for(int x = 0; x < 1;)
-            {
-                Vector3 searchNode = new Vector3(Random.Range(minX, maxX), 
-                                            motor.transform.position.y, 
-                                            Random.Range(minY, maxY));
-                
-            }
-        }*/
+        searchRoute.Add(predictedPlayerPosition);
+        searchRoute.Add(predictedPlayerPosition + (Vector3.right * 10));
+        searchRoute.Add(predictedPlayerPosition + (Vector3.right * -10));
+        searchRoute.Add(predictedPlayerPosition + (Vector3.forward * 10));
+        searchRoute.Add(predictedPlayerPosition + (Vector3.forward * -10));
 
         return searchRoute;
     }
