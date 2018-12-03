4e99262996cbd8dea749808a1d87b5ec547b38f2
Hayden Donnelly <Bizbud@live.com>
2018-12-03 12:21:22 -0500

    Key/Door System

---
+++ Assets/Scripts/PlayerInteraction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour 
+{
+	[SerializeField] private GameObject interactionPrompt;
+	private bool canInteract;
+	private GameObject interactableObject;
+
+	private void OnTriggerEnter(Collider col)
+	{
+		if(col.tag == "Interactable")
+		{
+			interactionPrompt.SetActive(true);
+			canInteract = true;
+			interactableObject = col.transform.gameObject;
+		}
+	}
+
+	private void OnTriggerExit(Collider col)
+	{
+		if(col.tag == "Interactable")
+		{
+			interactionPrompt.SetActive(false);
+			canInteract = false;
+		}
+	}
+
+	private void Update()
+	{
+		if(canInteract && Input.GetKeyDown(KeyCode.E))
+		{
+			interactableObject.SendMessage("Interact");
+		}
+	}
+}
