b602a5fdb96b094592bc55e1b18278c2f1310cc5
hayden-donnelly <hayden.git@proton.me>
2018-12-03 19:00:30 -0500

    MainMenu

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour 
 {
+	private bool doorUnlocked;
 	public void Interact()
 	{
-		if(GameState.hasKey)
+		if(GameState.hasKey && !doorUnlocked)
 		{
 			Debug.Log("Level complete");
-			// Load next level
+			doorUnlocked = true;
+			GameState.currentSceneID++;
+			SceneManager.LoadScene(GameState.currentSceneID);
 		}
 	}
 }
