375f37f28ad3b464ae18124d51cb68f4d337b04c
hayden-donnelly <hayden.git@proton.me>
2018-12-03 20:56:34 -0500

    MUSIC!

---
+++ Assets/Scripts/InitializeAudio.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitializeAudio : MonoBehaviour 
+{
+	[SerializeField] private AudioSource audioSource;
+	private bool FirstTime = true;
+
+	private void Start()
+	{
+		DontDestroyOnLoad(this.gameObject);
+
+		if(GameObject.FindGameObjectsWithTag("AudioListener").Length == 1) 
+		{
+			audioSource.Play();
+			FirstTime = false;
+		}
+		else
+		{
+			Destroy(this.gameObject);
+		}
+	}
+}
