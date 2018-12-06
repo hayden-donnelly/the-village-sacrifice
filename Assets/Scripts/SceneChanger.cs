c5b6ce4aa9ea6492614ca11371d16546cc5c57a8
hayden-donnelly <hayden.git@proton.me>
2018-12-05 23:31:30 -0500

    tweaks

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour 
+{
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Alpha0))
+		{
+			SceneManager.LoadScene(2);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SceneManager.LoadScene(3);
+		}
+		else if(Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			SceneManager.LoadScene(4);
+		}
+	}
+}
