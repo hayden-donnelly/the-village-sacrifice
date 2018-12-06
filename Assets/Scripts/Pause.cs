d7c05be47d9ac874fad7b27c3ef658317d1f1090
Hayden Donnelly <Bizbud@live.com>
2018-12-05 23:31:30 -0500

    tweaks

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour 
+{
+	private bool paused;
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.F))
+		{
+			if(paused)
+			{
+				paused = false;
+				Time.timeScale = 1;
+			}
+			else
+			{
+				paused = true;
+				Time.timeScale = 0;
+			}
+		}
+	}
+}
